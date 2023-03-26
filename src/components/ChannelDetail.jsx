a7dd158d9d53a66b2f70dece53e46932c502999a
David Tassama <davidtassama464@gmail.com>
2023-03-26 14:04:35 +0300

    Channel Detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
-export default ChannelDetail
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(
+        `search?channelId=${id}&part=snippet%2Cid&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
+  return <div>ChannelDetail</div>;
+};
+
+export default ChannelDetail;
